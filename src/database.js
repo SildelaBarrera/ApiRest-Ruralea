f181f20e47ac1acbb0dafc36366f3bbc03d3252f
CRBox <dharianfenix@gmail.com>
2024-05-06 17:23:07 +0200

    ajustes para uso en vercel

---
+++ src/database.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host:       process.env.DB_HOST         || "localhost",
+    user:       process.env.DB_USER         || "root",
+    password:   process.env.DB_PASSWORD     || "",
+    database:   process.env.DB_NAME         || "angular",
+    port:       process.env.DB_PORT         || 3306
+})
+
+console.log("Conexion correcta");
+
+module.exports = {connection};
