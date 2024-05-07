3c2764a505c34b05a1123ff2a57b5c6a3ffab747
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-05-07 11:52:11 +0200

    prueba vercel

--- src/controller/usuario.controller.js
+++
@@ -1,8 +0,0 @@
-
-function getStart(request, response)
-{
-    let respuesta = {error: true, codigo: 200, mensaje: 'Punto de inicio'};
-    response.send(respuesta);
-}
-
-module.exports = {getStart}
