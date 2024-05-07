170fb8c88a3aa5d72a9a69f6915ce613dd08775e
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-05-07 10:22:41 +0200

    commit cambios db

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host:       process.env.DB_HOST         || "angular.ctw08waqu7zb.us-east-1.rds.amazonaws.com",
-    user:       process.env.DB_USER         || "root",
+    user:       process.env.DB_USER         || "admin",
     password:   process.env.DB_PASSWORD     || "Ruralea0605.",
     database:   process.env.DB_NAME         || "angular",
     port:       process.env.DB_PORT         || 3306
