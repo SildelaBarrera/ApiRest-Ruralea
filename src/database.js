28ddc8cbc226e109d4492dc2daa845f0acc0184c
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-05-07 09:44:38 +0200

    commit vercel

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host:       process.env.DB_HOST         || "localhost",
     user:       process.env.DB_USER         || "root",
-    password:   process.env.DB_PASSWORD     || "",
+    password:   process.env.DB_PASSWORD     || "ruralea0605",
     database:   process.env.DB_NAME         || "angular",
     port:       process.env.DB_PORT         || 3306
 })
